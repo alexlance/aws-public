bdc40e75ee3c9b3ff9b2631f4272ec805baeb5af
Alex Lance <code@alexlance.com>
2024-09-29 08:33:59 +1000

    Add test

--- test-infra/main.tf
+++ test-infra/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "main" {
     instance_type = "${var.instance_type}"
     associate_public_ip_address = true
     root_block_device {
-        volume_size = 20
+        volume_size = 30
         volume_type = "gp2"
     }
 }
